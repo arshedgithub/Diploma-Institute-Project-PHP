dc00f11617054e4ba12e516319d096bfa12063c7
arshedgithub <arshedahmedmail@gmail.com>
2024-09-07 10:01:16 +0530

    redirect to login

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 
-    include_once("./templates/login.php");
-    // include_once("./templates/home.php");
-    
+    session_start();
+
+    // redirect to login page if not logged in
+    if (isset($_SESSION['username'])) {
+        header("Location: ./templates/home.php");
+    } else {
+        header("Location: ./templates/login.php");
+    }
+
+    exit();
+
 ?>
